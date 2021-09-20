35f6dca528ecbd43d421f26c39533508a703d5cf
SteffenJoergensen <crz@crz.dk>
2021-09-20 22:40:56 +0200

    Cleaner solution added.

--- easy/9_palindrome_number.py
+++ easy/9_palindrome_number.py
@@ -1,7 +1,7 @@
 def palindrome_number(x):
     x = str(x)
 
-    return x[:int(len(x) / 2)] == x[int(len(x) / 2 + (len(x) % 2)):][::-1]
+    return x == x[::-1]
 
 print(palindrome_number(121))
 print(palindrome_number(-121))
