3aba9b497d1f656976f2e5efc66c2494a196b732
SteffenJoergensen <crz@crz.dk>
2021-09-20 19:48:41 +0200

    Added a O(n) hashing solution.

--- easy/1_two_sum.py
+++ easy/1_two_sum.py
@@ -4,6 +4,14 @@ def two_sum(nums, target):
             if frst + scnd == target:
                 return [i, j+i+1]
 
-print(two_sum([2, 7, 11, 15], 9))
-print(two_sum([3, 2, 4], 6))
-print(two_sum([3, 3], 6))
+def two_sum_hashing(nums, target):
+    hash_table = {}
+    for i in range(len(nums)):
+        if target - nums[i] in hash_table:
+            return [hash_table[target - nums[i]], i]
+        else:
+            hash_table[nums[i]] = i
+
+print(two_sum_hashing([2, 7, 11, 15], 9))
+print(two_sum_hashing([3, 2, 4], 6))
+print(two_sum_hashing([3, 3], 6))
